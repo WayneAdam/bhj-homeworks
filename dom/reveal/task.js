d4eae0150166e655bbb931dabdae9664230029e6
Wayne <meibeat@mail.ru>
2023-08-23 15:26:34 +0300

    Добавил в обработку второй элемент

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,14 +1,22 @@
-const reveal = document.querySelector('.reveal');
+const reveal = document.querySelectorAll('.reveal');
 
 addEventListener('scroll', (event) => { // отслеживает изменение прокрутки окна
   const innerHeight = window.innerHeight; // получает высоту viewport
-  const y = reveal.getBoundingClientRect().y; // Y-координата элемента
-  const bottom = reveal.getBoundingClientRect().bottom; // координаты нижней границы элемента
+  const yElementFirst = reveal[0].getBoundingClientRect().y; // Y-координата элемента первого элемента
+  const yElementSecond = reveal[1].getBoundingClientRect().y; // Y-координата элемента второго элемента
+  const bottomElementFirst = reveal[0].getBoundingClientRect().bottom; // координаты нижней границы первого элемента
+  const bottomElementSecond = reveal[1].getBoundingClientRect().bottom; // координаты нижней границы второго элемента
 
-  if(y >= 0 && bottom <= innerHeight) {
-    reveal.classList.add('reveal_active');
+  if(yElementFirst >= 0 && bottomElementFirst <= innerHeight) {
+    reveal[0].classList.add('reveal_active');
   } else {
-    reveal.classList.remove('reveal_active');
+    reveal[0].classList.remove('reveal_active');
+  }
+
+  if(yElementSecond >= 0 && bottomElementSecond <= innerHeight) {
+    reveal[1].classList.add('reveal_active');
+  } else {
+    reveal[1].classList.remove('reveal_active');
   }
 
 });

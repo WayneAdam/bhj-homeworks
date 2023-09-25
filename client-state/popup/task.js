85bb8326b7464ec7ac5af1bb3680e6ace0f61642
Wayne <meibeat@mail.ru>
2023-09-25 21:49:08 +0300

    Initial commit. ДЗ-Всплывающее окно выполнено

---
+++ client-state/popup/task.js
@@ -0,0 +1,24 @@
+const modal = document.querySelector('#subscribe-modal');
+const modalCloseTimes = modal.querySelector('.modal__close');
+
+let cookie = document.cookie;
+
+function checkCookie() {
+  let cookieArray = cookie.split('; ');
+
+  for(let item of cookieArray) {
+    if(item !== 'modalclosed=true') {
+      modal.classList.add('modal_active');
+    };
+  };
+};
+checkCookie();
+
+function closeModal(event) {
+  event.preventDefault();
+
+  modal.classList.remove('modal_active');
+  document.cookie = 'modalclosed=true';
+};
+
+modalCloseTimes.addEventListener('click', closeModal);
